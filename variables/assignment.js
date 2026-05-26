331f6c2194466078c0080bc716e0fe87b601e138
lekrau <206306997+lekrau@users.noreply.github.com>
2026-05-26 07:45:10 +0200

    Complete variables and operators assignments
    https://www.theodinproject.com/lessons/foundations-variables-and-operators#assignment

---
+++ variables/assignment.js
@@ -0,0 +1,23 @@
+//Assignment 1
+console.log(23 + 97);
+
+//Assignment 2
+console.log(1 + 1 + 1 + 1 + 1 + 1);
+
+//Assignment 3
+console.log((4 + 6 + 9) / 77);
+
+//Assignment 4
+let a = 10;
+console.log(a);
+a = a - 1;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+
+//Assignment 5
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
