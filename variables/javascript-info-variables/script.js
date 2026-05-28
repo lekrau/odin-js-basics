45647b8a89dad288f378f3a5afed1f50923f388c
lekrau <206306997+lekrau@users.noreply.github.com>
2026-05-28 07:19:08 +0200

    Complete https://javascript.info/variables tasks

---
+++ variables/javascript-info-variables/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Task Working with variables https://javascript.info/task/hello-variables
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+// Task Giving the right name https://javascript.info/task/declare-variables
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+// Task Uppercase const? https://javascript.info/variables#uppercase-const
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
