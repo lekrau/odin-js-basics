4f400c661d9a1049dc7d877da6b8cfb01ccd207c
lekrau <206306997+lekrau@users.noreply.github.com>
2026-05-30 12:09:43 +0200

    Solve color choices with switch

--- conditionals/mdn-more-color-choices-switch/script.js
+++ conditionals/mdn-more-color-choices-switch/script.js
@@ -2,9 +2,31 @@ const select = document.querySelector("select");
 const html = document.querySelector("html");
 
 select.addEventListener("change", () => {
-  const choice = select.value;
+    const choice = select.value;
+    let bgColor = "white";
+    let textColor = "black";
 
   // ADD SWITCH STATEMENT
+  switch (choice) {
+    case "black":
+        bgColor = "black";
+        textColor = "white";
+        break;
+    case "purple":
+        bgColor = "purple";
+        textColor = "#008000";
+        break;
+    case "yellow":
+        bgColor = "yellow";
+        textColor = "#0000FF";
+        break;
+    case "psychedelic":
+        bgColor = "#E713EF";
+        textColor = "#1812FA";
+        break;
+  }
+  
+  update(bgColor, textColor);
 });
 
 function update(bgColor, textColor) {
