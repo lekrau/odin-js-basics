8865f31d1225489a91beaecb89e70e4cf26d6084
lekrau <206306997+lekrau@users.noreply.github.com>
2026-05-30 11:33:58 +0200

    Set up basic calendar task files

---
+++ conditionals/mdn-basic-calendar-conditionals/script.js
@@ -0,0 +1,25 @@
+const select = document.querySelector("select");
+const list = document.querySelector("ul");
+const h1 = document.querySelector("h1");
+
+select.addEventListener("change", () => {
+  const choice = select.value;
+  createCalendar(choice);
+});
+
+function createCalendar(month) {
+  let days = 31;
+
+  // ADD CONDITIONAL HERE
+
+  list.textContent = "";
+  h1.textContent = month;
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+}
+
+select.value = "January";
+createCalendar("January");
