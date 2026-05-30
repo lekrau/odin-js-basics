1828b29c6a7b110cf3c2300b0557c8aedefffd9c
lekrau <206306997+lekrau@users.noreply.github.com>
2026-05-30 11:42:26 +0200

    Refactor calendar logic to use if else

--- conditionals/mdn-basic-calendar-conditionals/script.js
+++ conditionals/mdn-basic-calendar-conditionals/script.js
@@ -3,47 +3,31 @@ const list = document.querySelector("ul");
 const h1 = document.querySelector("h1");
 
 select.addEventListener("change", () => {
-  const choice = select.value;
-  createCalendar(choice);
+    const choice = select.value;
+    createCalendar(choice);
 });
 
 function createCalendar(month) {
-  let days = 31;
+    let days = 31;
 
-  switch (month) {
-  case "February":
-    days = 28;
-    break;
+    if (month === "February") {
+        days = 28;
+    } else if (
+        month === "April" ||
+        month === "June" ||
+        month === "September" ||
+        month === "November"
+    ) {
+        days = 30;
+    }
 
-  case "April":
-    days = 30;
-    break;
-
-  case "June":
-    days = 30;
-    break;
-
-  case "September":
-    days = 30;
-    break;
-
-  case "November":
-    days = 30;
-    break;
-
-  default:
-    // Redundant
-    days = 31;
-    break;
-  }
-
-  list.textContent = "";
-  h1.textContent = month;
-  for (let i = 1; i <= days; i++) {
-    const listItem = document.createElement("li");
-    listItem.textContent = i;
-    list.appendChild(listItem);
-  }
+    list.textContent = "";
+    h1.textContent = month;
+    for (let i = 1; i <= days; i++) {
+        const listItem = document.createElement("li");
+        listItem.textContent = i;
+        list.appendChild(listItem);
+    }
 }
 
 select.value = "January";
