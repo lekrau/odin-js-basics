70368accff76e56f703a1c798cafeca84115c053
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-10 07:49:10 +0200

    Solve MDN launch countdown exercise

--- loops-and-arrays/mdn-launch-countdown/script.js
+++ loops-and-arrays/mdn-launch-countdown/script.js
@@ -1,30 +1,5 @@
 // Exercise: MDN Implementing a launch countdown
-
-// Problem Description in My Own Words
-// TBD
-
-// Input: TBD
-// Output: TBD
-
-// Assumptions
-// - TBD
-
-// Test Cases (2 - 4)
-// Normal case:
-// FUNCTION(ARGUMENTS) -> RESULT
-// Edge / boundary case:
-// FUNCTION(ARGUMENTS) -> RESULT
-// OPTIONAL Case related to assumptions:
-// FUNCTION(ARGUMENTS) -> RESULT
-// OPTIONAL Out of scope:
-// FUNCTION(ARGUMENTS) -> no valid result
-
-// Rough Plan / Subproblems
-// - TBD
-// Next Step (Smallest Subproblem): TBD
-
-// Pseudocode for the Next Step
-// TBD
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Loops#implementing_a_launch_countdown
 
 // Implementation
 "use strict";
@@ -32,14 +7,14 @@
 const output = document.querySelector(".output");
 output.textContent = "";
 
-// let i = 10;
-
-// const para = document.createElement('p');
-// para.textContent = ;
-// output.appendChild(para);
-
-
-// Short Debug Log
-// What went wrong, if anything?
-// Which assumption was tested or confirmed?
-// What did I change or learn?
+for (let i = 10; i >= 0; i--) {
+    const para = document.createElement('p');
+    if (i === 10) {
+        para.textContent = "Countdown " + i;
+    } else if (i === 0) {
+        para.textContent = "Blast off!";
+    } else {
+        para.textContent = i;
+    }
+    output.appendChild(para);
+}
