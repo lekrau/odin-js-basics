2dd4abd1afc33c599da401eb6b308e1e42b12ad6
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-10 07:54:54 +0200

    Set up MDN guest list exercise

---
+++ loops-and-arrays/mdn-guest-list/script.js
@@ -0,0 +1,27 @@
+// Exercise: MDN Filling in a guest list
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Loops#filling_in_a_guest_list
+
+// Implementation
+"use strict";
+
+const people = [
+  "Chris",
+  "Anne",
+  "Colin",
+  "Terri",
+  "Phil",
+  "Lola",
+  "Sam",
+  "Kay",
+  "Bruce",
+];
+
+const admitted = document.querySelector(".admitted");
+const refused = document.querySelector(".refused");
+admitted.textContent = "Admit: ";
+refused.textContent = "Refuse: ";
+
+// loop starts here
+
+// refused.textContent += ...;
+// admitted.textContent += ...;
